f9e423bcffc2b027cccd45d388e8088f558006dd
agb0607 <agb0607@pribit.com>
2025-05-12 09:54:32 +0900

    test

--- Package.swift
+++ Package.swift
@@ -12,12 +12,15 @@ let package = Package(
             name: "DetectTouchWindow",
             targets: ["DetectTouchWindow"]),
     ],
+    dependencies: [
+        .package(path: "../WebSocket/WebSocketModule"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "DetectTouchWindow",
-            dependencies: ["DetectTouchObservableObject"]),
+            dependencies: ["DetectTouchObservableObject", "WebSocketModule"]),
         .target(
             name: "DetectTouchObservableObject"),
         .testTarget(
